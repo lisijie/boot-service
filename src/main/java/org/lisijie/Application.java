5bee33cb4e97f1c94c5437d457909f5511033d16
sijie.li <lsj86@qq.com>
2016-06-13 12:03:25 +0800

    支持声明式事务

--- src/main/java/org/lisijie/Application.java
+++ src/main/java/org/lisijie/Application.java
@@ -3,7 +3,9 @@ package org.lisijie;
 import org.lisijie.thrift.annotation.EnableThrift;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @EnableThrift
 public class Application {
